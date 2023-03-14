b27045df40a86df40133eb51024b4ad65df64756
Xiangyu Wang <wxyucs@gmail.com>
2023-03-14 15:06:47 +0800

    update treemacs

--- lisp/init-treemacs.el
+++ lisp/init-treemacs.el
@@ -15,11 +15,13 @@
 (require 'treemacs-evil)
 (require 'treemacs-projectile)
 
+(setq treemacs-display-current-project-exclusively t)
 (add-hook 'emacs-startup-hook 'treemacs)
 (treemacs-tag-follow-mode)
 
 (global-set-key (kbd "C-x t t") 'treemacs)
 (global-set-key (kbd "C-x t g") 'treemacs-select-window)
+(global-set-key (kbd "C-x t a") 'treemacs-add-and-display-current-project)
 
 (provide 'init-treemacs)
 ;;; init-treemacs.el ends here
