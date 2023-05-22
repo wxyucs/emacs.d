642e0d4c1c4f10fb9d4159c26de4ed8890cdcd6c
Xiangyu Wang <wxyucs@gmail.com>
2023-05-22 10:53:41 +0800

    hide menubar, toolbar and scrollbar

--- lisp/init-window.el
+++ lisp/init-window.el
@@ -4,6 +4,9 @@
 
 (setq display-line-numbers-type 'relative)
 (global-display-line-numbers-mode)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
 
 (provide 'init-window)
 ;;; init-window.el ends here
